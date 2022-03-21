478851a73a71040d11f3aa01ccfa10982ea1b924
Naufal Azim <naufal.azim@outlook.com>
2022-03-22 01:24:22 +0800

    first

---
+++ src/main/java/com/project/musicappbackend/MusicAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.musicappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/musicappbackend/MusicAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.musicappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
